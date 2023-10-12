419da718c72d2e805b18cddfb9558f981546abe9
FatimaAli234 <144320660+FatimaAli234@users.noreply.github.com>
2023-10-12 21:22:30 +0400

    chapter 1 exercise 4

---
+++ Chapter 1- Getting Started with Python/Exercises/Exercise4.py
@@ -0,0 +1,6 @@
+#Write three strings in different variables and print the output as one string.
+greeting="Hello,"
+target="world!"
+question="How are you?"
+combined_string= greeting + target + question
+print("combined string:",combined_string)
