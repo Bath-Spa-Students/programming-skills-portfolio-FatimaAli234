4649c0428064f1d6d0c00805f47f6527c4435c1c
FatimaAli234 <144320660+FatimaAli234@users.noreply.github.com>
2023-11-05 00:39:51 +0400

    Exercise2.py

---
+++ Chapter 5- Dictionaries/Exercises/Exercise2.py
@@ -0,0 +1,11 @@
+
+Glossary={
+    "Variable":" symbolic name given to an unknown quantity that permits the name to be used independent of the information it represents",
+    "Function":"a block of code that performs a task.",
+    "List":"An ordered collection of items that can be of different data types.",
+    "Dictionary":"A key-value data structure used to store and organize information.",
+    "String": "A sequence of characters, often used to represent text, enclosed in single or double quotes.",
+}
+  
+for word, defination in Glossary.items():
+    print(f"{word}:\n{defination}\n")
