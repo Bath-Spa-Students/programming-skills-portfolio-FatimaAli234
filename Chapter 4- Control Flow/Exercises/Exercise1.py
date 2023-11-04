724f76bc78e5e0c3115e8343f27813f4ab94bbe9
FatimaAli234 <144320660+FatimaAli234@users.noreply.github.com>
2023-11-04 22:38:36 +0400

    ch 4 exercise 1

---
+++ Chapter 4- Control Flow/Exercises/Exercise1.py
@@ -0,0 +1,14 @@
+
+
+alien_colors = 'green'
+
+if alien_colors=='green':
+    print("The player just earned 5 points")
+
+
+#Another test 
+Alien_color='yellow'
+if Alien_color=='green':
+    print("The player just earned 5 points")
+    
+    
