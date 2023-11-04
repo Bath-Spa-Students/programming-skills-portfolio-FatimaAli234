77a018f8f048a05b62eeabe4b4617ab9b5ba8fd8
FatimaAli234 <144320660+FatimaAli234@users.noreply.github.com>
2023-11-05 00:39:26 +0400

    exercise 1

---
+++ Chapter 5- Dictionaries/Exercises/Exercise1.py
@@ -0,0 +1,13 @@
+
+Dictionary={
+   "First_name":"Sara",
+   "Last_name":"Ali",
+   "Age":"20",
+   "City":"Dubai"
+   
+}
+ 
+print("First name:",Dictionary["First_name"])
+print("Last name:",Dictionary["Last_name"])
+print("Age:",Dictionary["Age"])
+print("city:",Dictionary["City"])
