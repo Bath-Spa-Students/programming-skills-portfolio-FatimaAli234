8e3318a8299c28581d313e7d9778fd90250b043d
FatimaAli234 <144320660+FatimaAli234@users.noreply.github.com>
2023-11-05 17:20:48 +0400

    Exercise5.py

---
+++ Chapter 5- Dictionaries/Exercises/Exercise5.py
@@ -0,0 +1,11 @@
+
+Pets=[
+    {"kind":"Cat", "Owner":"Fatima"},
+    {"kind":"snake","Owner":"Sara"},
+    {"kind":"Fish","Owner":"Alisa"},
+    {"kind":"Dog","Owner":"lana"},
+]
+
+for Animal in Pets:
+    print(f"kind of Animal:{Animal['kind']}")
+    print(f"Owners name:{Animal['Owner']}")
