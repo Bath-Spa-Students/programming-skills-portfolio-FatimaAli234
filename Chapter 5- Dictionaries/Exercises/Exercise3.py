ee53aea66dd2ceeaeab9bcaadd4f11ef5fae06ba
FatimaAli234 <144320660+FatimaAli234@users.noreply.github.com>
2023-11-05 16:23:37 +0400

    Exercise3.py

---
+++ Chapter 5- Dictionaries/Exercises/Exercise3.py
@@ -0,0 +1,17 @@
+
+Glossary={
+    "Variable":" symbolic name given to an unknown quantity that permits the name to be used independent of the information it represents",
+    "Function":"a block of code that performs a task.",
+    "List":"An ordered collection of items that can be of different data types.",
+    "Dictionary":"A key-value data structure used to store and organize information.",
+    "String": "A sequence of characters, often used to represent text, enclosed in single or double quotes.",
+}
+
+
+Glossary["Tuple"] = "An immutable ordered collection of items, usually enclosed in parentheses."
+Glossary["Loop"] = "A control structure which repeatedly executes the block of code."
+Glossary["Syntax"] = "A set of rules that dictate the combinations of keywords and symbols used in a programming language."
+Glossary["Module"] = "A file containing Python code that can be imported and used in other programs."
+Glossary["Algorithm"] = "A step-by-step procedure or set of rules for solving a specific problem or accomplishing a particular task in a finite number of steps."
+for word, defination in Glossary.items():
+    print(f"{word}:\n{defination}\n")
