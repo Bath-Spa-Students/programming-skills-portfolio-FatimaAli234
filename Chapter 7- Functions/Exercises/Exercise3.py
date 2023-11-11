b7e44b118ea10d2e50635615386745c9e2b77b0c
FatimaAli234 <144320660+FatimaAli234@users.noreply.github.com>
2023-11-12 00:29:47 +0400

    exercise3.py

---
+++ Chapter 7- Functions/Exercises/Exercise3.py
@@ -0,0 +1,5 @@
+
+def make_shirt(size, message):
+    print(f"Making a shirt of size {size} with the message {message}.")
+make_shirt("Small", "Hi everyone!")
+make_shirt(size="Medium", message="I like Python")
