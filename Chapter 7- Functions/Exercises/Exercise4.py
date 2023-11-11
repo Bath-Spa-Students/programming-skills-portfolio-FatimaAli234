fdd265f4731c1d9ca07c0cc481fb1b24822b87f5
FatimaAli234 <144320660+FatimaAli234@users.noreply.github.com>
2023-11-12 00:30:02 +0400

    exercise4.py

---
+++ Chapter 7- Functions/Exercises/Exercise4.py
@@ -0,0 +1,6 @@
+
+def make_shirt(size="large", message="I love Python"):
+    print(f"Making a shirt of size {size} with the message: '{message}'.")
+make_shirt()
+make_shirt(size="small")
+make_shirt(size="medium", message="Coding is great!")
