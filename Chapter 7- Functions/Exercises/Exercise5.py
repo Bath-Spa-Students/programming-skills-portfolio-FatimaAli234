34467d18f5302f966ddadc61200293c2b8fe683d
FatimaAli234 <144320660+FatimaAli234@users.noreply.github.com>
2023-11-12 00:30:17 +0400

    exercise5.py

---
+++ Chapter 7- Functions/Exercises/Exercise5.py
@@ -0,0 +1,6 @@
+
+def describe_city(city,country="default country"):
+    print(f"{city} is in {country}")
+describe_city("Sydney","Australia")
+describe_city("New york","America")
+describe_city("Tokyo")
