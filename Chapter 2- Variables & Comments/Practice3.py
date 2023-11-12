1f4e89c5695e9c0cb77c88c1a76a2d070c5cbfe9
FatimaAli234 <144320660+FatimaAli234@users.noreply.github.com>
2023-11-12 15:57:18 +0400

    practice3.py

---
+++ Chapter 2- Variables & Comments/Practice3.py
@@ -0,0 +1,9 @@
+User_input = input("Enter your Value here:")
+String = str(User_input)
+integer = int(User_input)
+Float = float(User_input)
+
+print("Original input:", User_input)
+print("String Variable:", String)
+print("Integer Variable:", integer)
+print("Float Variable:", Float)
