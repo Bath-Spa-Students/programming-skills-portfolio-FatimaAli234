73470e217d66cfe4ccc4e0e66b143bc2c4e7026b
FatimaAli234 <144320660+FatimaAli234@users.noreply.github.com>
2023-11-13 19:40:13 +0400

    practice1.py

---
+++ Chapter 7- Functions/Exercises/practice1.py
@@ -0,0 +1,9 @@
+#Write a Python function that takes two numbers as arguments and returns their su
+
+def add_num(b,c):
+    return b+c
+num1=14
+num2=20
+#Adding
+result=add_num(num1,num2)
+print(f"The sum of {num1} and {num2} is {result}")
