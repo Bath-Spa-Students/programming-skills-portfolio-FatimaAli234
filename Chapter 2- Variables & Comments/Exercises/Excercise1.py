1eca1bd045a34a9e27ac670000a292a73d066c31
FatimaAli234 <144320660+FatimaAli234@users.noreply.github.com>
2023-11-13 22:42:35 +0400

    Exercise1.py

--- Chapter 2- Variables & Comments/Exercises/Excercise1.py
+++ Chapter 2- Variables & Comments/Exercises/Excercise1.py
@@ -1,4 +1,6 @@
 
+#Assign a message to a variable, and print that message.Then change the value of the variable to a new message, and print the newmessage.
+
 Name="Fatima"
 print(Name)
 My_name="How are you?"
