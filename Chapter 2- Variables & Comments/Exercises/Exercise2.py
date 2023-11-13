cf772e942cc313f64bcf44e8c5026d17e5cd7a77
FatimaAli234 <144320660+FatimaAli234@users.noreply.github.com>
2023-11-13 22:43:59 +0400

    Exercise2.py

--- Chapter 2- Variables & Comments/Exercises/Exercise2.py
+++ Chapter 2- Variables & Comments/Exercises/Exercise2.py
@@ -1,3 +1,6 @@
+#Find a quote from a famous person you admire. Print the quote and the name of its author. 
+#Your output should look something like the following, including the quotation marks:
+#Albert Einstein once said, “A person who never made a mistake never tried anything new.
 
 quote="Success is not final, failure is not fatal: It is the courage to continue that counts."
 Author="Winsten churchill"
