305442bd7dfd88ab0be7c37d66ab7ad6fe83a83c
FatimaAli234 <144320660+FatimaAli234@users.noreply.github.com>
2023-11-14 17:27:31 +0400

    practice3.py

--- Chapter 3- Data Structures/Exercises/practice3.py
+++ Chapter 3- Data Structures/Exercises/practice3.py
@@ -1,4 +1,5 @@
-
+#Assume names references a list. Write a for loop that displays each element of the list.
+# 
 names_list=["Fatima","Hafsa","Sara","Mariam"]
 for name in names_list:
     print(names_list)
