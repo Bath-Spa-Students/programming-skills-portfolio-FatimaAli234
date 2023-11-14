7c076f5ab0624b8ba6f26b0a3cc216d0f6952ba6
FatimaAli234 <144320660+FatimaAli234@users.noreply.github.com>
2023-11-14 18:04:17 +0400

    Exercise5.py

--- Chapter 5- Dictionaries/Exercises/Exercise5.py
+++ Chapter 5- Dictionaries/Exercises/Exercise5.py
@@ -1,3 +1,6 @@
+#Make several dictionaries, where each dictionary represents a different pet. In each dictionary, include the kind of animal and the
+# owner’s name. Store these dictionaries in a list called pets. Next, loop through your list and asyou do, print everything you know about 
+# each pet
 
 Pets=[
     {"kind":"Cat", "Owner":"Fatima"},
