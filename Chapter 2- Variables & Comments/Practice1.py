f01b50e077f1ceed3c9c6ffee11da73e0b371d37
FatimaAli234 <144320660+FatimaAli234@users.noreply.github.com>
2023-11-14 15:06:06 +0400

    practice1.py

--- Chapter 2- Variables & Comments/Practice1.py
+++ Chapter 2- Variables & Comments/Practice1.py
@@ -1,6 +1,10 @@
+#Write a python code that uses three variables x, y, and z with respective values 10,20 and 30. Add all the 
+#variables and then store the result in variable a. Print the value of a.
 
+#assign values to variables
 X=10
 y=20
 z=30
+#Adding the variables and storing the result in variable a.
 a=X+y+z
 print(a)
