0442792b77c743f7bf542349b77392460345a76e
FatimaAli234 <144320660+FatimaAli234@users.noreply.github.com>
2023-11-14 15:04:07 +0400

    Exercise4.py

--- Chapter 2- Variables & Comments/Exercises/Exercise4.py
+++ Chapter 2- Variables & Comments/Exercises/Exercise4.py
@@ -1,3 +1,5 @@
+#Use a variable to represent your favorite number. Then,using that variable, create a message that reveals your favorite number. Print
+#that message.
 
 Favourite_number=28
 number="My favourite number is " + str(Favourite_number)
