41609d2630f43039a1246bdb8cbd5fb8ce0815ea
FatimaAli234 <144320660+FatimaAli234@users.noreply.github.com>
2023-11-14 15:06:18 +0400

    practice2.py

--- Chapter 2- Variables & Comments/Practice2.py
+++ Chapter 2- Variables & Comments/Practice2.py
@@ -1,10 +1,9 @@
+#Write a python program that takes courses marks as input and then calculates average of all the 
+#courses. After calculating the average, calculate the percentage of a student using total marks. Assume 
+#the total of all the courses marks is 500 or take the total marks from the user as input.
 
 Courses= int(input("Enter the number of courses: "))
-
-
 total_marks = float(input("Enter the total marks: "))
-
-
 sum_of_marks = 0
 
 for i in range(Courses):
