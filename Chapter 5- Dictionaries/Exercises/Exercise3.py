8d5998b91df4304059e685b1937c126b8e41e65d
FatimaAli234 <144320660+FatimaAli234@users.noreply.github.com>
2023-11-14 18:03:46 +0400

    Exercise3.py

--- Chapter 5- Dictionaries/Exercises/Exercise3.py
+++ Chapter 5- Dictionaries/Exercises/Exercise3.py
@@ -1,9 +1,12 @@
+#Now that you know how to loop through a dictionary, clean up the code from Exercise 6-3 (page 99) by replacing your series of print()
+#calls with a loop that runs through the dictionary’s keys and values. When you’re sure that your loop works, add five more Python terms 
+#to your glossary.When you run your program again, these new words and meanings should automatically be included in the output
 
 Glossary={
     "Variable":" symbolic name given to an unknown quantity that permits the name to be used independent of the information it represents",
     "Function":"a block of code that performs a task.",
     "List":"An ordered collection of items that can be of different data types.",
-    "Dictionary":"A key-value data structure used to store and organize information.",
+    "Dictionary":"A key-value data structure used to store and organixe information.",
     "String": "A sequence of characters, often used to represent text, enclosed in single or double quotes.",
 }
 
