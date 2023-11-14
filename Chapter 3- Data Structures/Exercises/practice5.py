dd9eceda48e485506e957018fae6c72cee549fa5
FatimaAli234 <144320660+FatimaAli234@users.noreply.github.com>
2023-11-14 17:32:21 +0400

    practice5.py

--- Chapter 3- Data Structures/Exercises/practice5.py
+++ Chapter 3- Data Structures/Exercises/practice5.py
@@ -1,3 +1,6 @@
+#You have given a Python list. Write a program to find value 20 in the list, and if it is present, 
+# replace it with 200. Only update the first occurrence of an item.Work with the given list:
+# list1 = [5, 10, 15, 20, 25, 50, 20]
 
 list= [5, 10, 15, 20, 25, 50, 20]
 list[list.index(20)]=200
