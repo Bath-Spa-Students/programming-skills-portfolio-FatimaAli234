f121ec102cc02e4ce22b6464e2d6b786b4af247a
FatimaAli234 <144320660+FatimaAli234@users.noreply.github.com>
2023-11-14 15:08:31 +0400

    practice4.py

--- Chapter 2- Variables & Comments/practice4.py
+++ Chapter 2- Variables & Comments/practice4.py
@@ -1,5 +1,10 @@
 
+#Write a python program that stores an integer and string value to variables x and y. Print the type of 
+#variable x and y
+
+#Assign value and string to variables x and y
 x=19
 y="Hello"
+#print the type of variable x and y
 print("Type of variable x:",type(x))
 print("Type of variable y:",type(y))
