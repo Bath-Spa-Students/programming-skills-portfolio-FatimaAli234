671cc1980650aa9482d97110702b552b55c89339
FatimaAli234 <144320660+FatimaAli234@users.noreply.github.com>
2023-11-14 18:22:17 +0400

    practice5.py

--- Chapter 6- Loops/Exercises/practice5.py
+++ Chapter 6- Loops/Exercises/practice5.py
@@ -1,9 +1,11 @@
+#Write a Python program that uses a while loop to find the largest number among a series of 
+#user-input values until they enter '0' to exit the loop.
+
 
 largest_num = None
 
 while True:
     user_input = input("Enter a number here (or type '0' to stop): ")
-
     if user_input == '0':
         break
     number = int(user_input)
